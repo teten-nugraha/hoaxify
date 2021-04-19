7284e390c681c217c2aef74efc2f03a3e1df5b92
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-04-19 08:46:57 +0700

    first

---
+++ src/main/java/id/ten/hoaxyfi/HoaxyfiApplication.java
@@ -0,0 +1,13 @@
+package id.ten.hoaxyfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoaxyfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoaxyfiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ten/hoaxyfi/HoaxyfiApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ten.hoaxyfi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoaxyfiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
